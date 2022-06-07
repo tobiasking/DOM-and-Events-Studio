e20ea554767a2cc47e0ab617ed0caefb731f4a87
toby king <tobyjacquesking@gmail.com>
2022-06-06 20:54:58 -0400

    added js edits

--- scripts.js
+++ scripts.js
@@ -1,2 +1,47 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+function init () {
+    let takeOff = document.getElementById("takeoff");
+    let flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    let landing = document.getElementById("landing");
+    let missionAbort = document.getElementById("missionAbort");
+    let up = document.getElementById("up");
+    let down = document.getElementById("down");
+    let right = document.getElementById("right");
+    let left = document.getElementById("left");
+    let objImage = document.getElementById("rocket");
+    objImage.style.position = "relative";
+    objImage.style.left = "0px";
+    objImage.style.top = "0px";
+
+
+takeOff.addEventListener("click", event => {
+let response = confirm("Is the rocket ready for launch?");
+if (response) {
+    flightStatus.innerHTML = "Shuttle in flight!";
+    shuttleBackground.style.backgroundColor = "blue";
+    let value = spaceShuttleHeight.innerHTML;
+    spaceShuttleHeight.innerHTML = Number(value) + 10000;
+}
+});
+landing.addEventListener("click", event => {
+    flightStatus.innerHTML = "The shuttle is landing. Landing gear engaged.";
+    shuttleBackground.style.backgroundColor = "green";
+    spaceShuttleHeight.innerHTML = 0;  
+});
+
+missionAbort.addEventListener("click", event =>{
+    let response = confirm("Are you sure you wish to abort?");
+    if (response) {
+        flightStatus.innerHTML = "Mission aborted.";
+        shuttleBackground.style.backgroundColor = "green";
+        spaceShuttleHeight.innerHTML = 0;
+    }
+});
+
+
+}
+window.addEventListener("load", init);
